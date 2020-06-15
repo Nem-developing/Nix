55b60a08750a8c87db0c055546589bd72946810c
Néhémie <54665744+Nem-developing@users.noreply.github.com>
2020-06-15 22:28:01 +0200

    Juste pour le style ^^.

--- config/config.php
+++ config/config.php
@@ -1,8 +1,10 @@
 <?php
 
-/* 
- * Veuillez spécifiez les informations de connection à votre base de donnée.
- */
+/*  
+ *  =========================================================================
+ *  Veuillez spécifiez les informations de connection à votre base de donnée.
+ *  =========================================================================
+ */ 
 
 $hotedeconnexion = "127.0.0.1"; // 127.0.0.1 = Localhost
 $basededonnee = "MW-SRV";
