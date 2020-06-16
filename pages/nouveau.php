c8f1d23fbc2f0bc25f6b3dfeea5abd493f528ddb
Néhémie <54665744+Nem-developing@users.noreply.github.com>
2020-06-16 23:10:44 +0200

    Changement lien.

--- pages/nouveau.php
+++ pages/nouveau.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-        <link href="css/create.css" rel="stylesheet" type="text/css"/>
+        <link href="css/nouveau.css" rel="stylesheet" type="text/css"/>
         <title>Création d'un nouveau serveur !</title>
     </head>
     <body>
