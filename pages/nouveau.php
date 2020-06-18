eeb75af9284a9803b0afb67cb95d24de677a2e1b
Néhémie <54665744+Nem-developing@users.noreply.github.com>
2020-06-18 22:38:51 +0200

    Ajout de la récupération d'IP.
    Cette @IP  est stockée sur la base de donné client uniquement elle n'est pas communiqué autre part.
    Cet ajout permet simplement de conserver une trace de l'utilisateur qui a créé le serveur.

--- pages/nouveau.php
+++ pages/nouveau.php
@@ -47,7 +47,13 @@
             $namesrv = $_POST['namesrv'];       // On récupère les informations du formulaire précédent.
             $version = $_POST['version'];       // On récupère les informations du formulaire précédent.
             $date = strftime("%d/%m/%y");       // On entre la date dans la variable $date.
+            $ip = $_SERVER['REMOTE_ADDR'];      // On récupère l'addresse IP du client. | Note : Cette IP est stoqué sur la base de donné client uniquement.
+            
             (int) $erreur = 0;
+            
+            if (!$ip) {
+                $ip = "0.0.0.0";    // Si l'utilisateur utilise un proxy ; La fonction Remote addr peut dysfonctionner ; C'est une mesure de sécurité.
+            }
 
             //  Connexion à la base de donnée.
             $mysqli = new mysqli("$hotedeconnexion", "$utilisateur", "$motdepasse", "$basededonnee");
@@ -58,14 +64,14 @@
             }
             // Création de la table où l'on stoque les informations de créations de serveurs.
             // Nous notons que le champ "Actif" permet de trier les serveur actifs (=0) de ceux qui sont supprimés(=1)
-            if (!$mysqli->query("CREATE TABLE IF NOT EXISTS `serveurs` ( `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT, `nom` varchar(21) NOT NULL, `version` varchar(7) NOT NULL, `datecreation` varchar(10) NOT NULL, `actif` int(1) NOT NULL );")) {
+            if (!$mysqli->query("CREATE TABLE IF NOT EXISTS `serveurs` ( `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT, `nom` varchar(21) NOT NULL, `version` varchar(7) NOT NULL, `datecreation` varchar(10) NOT NULL, `ipcrea` varchar(19) NOT NULL,  `actif` int(1) NOT NULL );")) {
                 echo "<div class='alert alert-danger' role='alert'> Echec lors de la création de la table serveurs ! </div>";    // Affichage de l'erreur.
                 echo "<div class='alert alert-danger' role='alert'> Erreur N°$mysqli->errno : $mysqli->error.</div>";    // Affichage de l'erreur.
                 $erreur = $erreur + 1;
             }
 
             // On ajoute les informations du formulaire dans la table "serveurs".
-            if (!$mysqli->query("INSERT INTO `serveurs` (`nom`, `version` , `datecreation`, `actif`) VALUES ('$namesrv', '$version', '$date', '0');")) {
+            if (!$mysqli->query("INSERT INTO `serveurs` (`nom`, `version` , `datecreation`, `ipcrea`, `actif`) VALUES ('$namesrv', '$version', '$date', '$ip', '0');")) {
                 echo "<div class='alert alert-danger' role='alert'> Echec lors de l'ajout de vos données dans de la table ! </div>";    // Affichage de l'erreur.
                 echo "<div class='alert alert-danger' role='alert'> Erreur N°$mysqli->errno : $mysqli->error.</div>";    // Affichage de l'erreur.
                 $erreur = $erreur + 1;
@@ -132,6 +138,12 @@
             } else {
                 echo "<h1>Il semble y avoir une erreur, veuillez vous référer à l'alerte au dessus !</h1>";
             }
+            
+            
+            
+            
+            
+            
             ?>
 
 
