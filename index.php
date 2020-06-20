d02a8394ca3ff3fffa6a586b3752b44485054073
Néhémie <54665744+Nem-developing@users.noreply.github.com>
2020-06-20 13:51:21 +0200

    Update index.php

--- index.php
+++ index.php
@@ -76,10 +76,11 @@
                     <li class='list-group-item'>Version : $ligne->version</li>
                     <li class='list-group-item'>ID : $ligne->id</li>
                 </ul>
-                <div class='card-body'> 
-                    <a href='#' class='card-link pencil'><svg class='bi bi-pencil' width='1em' height='1em' viewBox='0 0 16 16' fill='currentColor' xmlns='http://www.w3.org/2000/svg'>
+                <div class='card-body'> <span class'pencil'>
+                    <a href='#' class='card-link'><svg class='bi bi-pencil' width='1em' height='1em' viewBox='0 0 16 16' fill='currentColor' xmlns='http://www.w3.org/2000/svg'>
                         <path fill-rule='evenodd' d='M11.293 1.293a1 1 0 0 1 1.414 0l2 2a1 1 0 0 1 0 1.414l-9 9a1 1 0 0 1-.39.242l-3 1a1 1 0 0 1-1.266-1.265l1-3a1 1 0 0 1 .242-.391l9-9zM12 2l2 2-9 9-3 1 1-3 9-9z'/>a
                         <path fill-rule='evenodd' d='M12.146 6.354l-2.5-2.5.708-.708 2.5 2.5-.707.708zM3 10v.5a.5.5 0 0 0 .5.5H4v.5a.5.5 0 0 0 .5.5H5v.5a.5.5 0 0 0 .5.5H6v-1.5a.5.5 0 0 0-.5-.5H5v-.5a.5.5 0 0 0-.5-.5H3z'/></svg>Modifier le serveur</a><br>
+                    </span>
                     <a href='#' class='btn btn-success'>Démarer</a>
                     <a href='#' class='btn btn-danger'>Eteindre</a>
                 </div>
