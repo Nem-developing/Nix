a53949834e3132702f4b007ca81c5bd1ec9aa688
Néhémie <54665744+Nem-developing@users.noreply.github.com>
2020-06-21 13:54:10 +0200

    Ajout des commandes de démarrage & d'extinction.

--- actions/lanceroustopper.php
+++ actions/lanceroustopper.php
@@ -6,15 +6,30 @@
     </head>
     <body>
         <?php
-        
         // Si un utilisateur tente d'accéder à cette page sans passer par les boutons adaptés, alors on va l'éjecter.
-        if (!$_GET['id'] or !$_GET['action']){
+        if (!$_GET['id'] or!$_GET['action']) {
             header('Location: ../index.php');   // redireciton vers la page d'acceuil.
             exit();
+        }
+
+        $id = $_GET['id'];
+        $action = $_GET['action'];
+
+        switch ($action) {
+            case "lancer":
+                echo "Démarrage du serveur";
+                
+                shell_exec("cd /home/mwsrv-user/$idserveur ;sh start_avec_screen.sh");
+                
+                break;
+            case "stopper":
+                echo "Extinction du serveur";
+                shell_exec("cd /home/mwsrv-user/$idserveur ; screen -S serveur_$id -p 0 -X stuff `printf 'stop\r'`");
+                break;
         }
         
         
         ?>
-        
+
     </body>
 </html>
