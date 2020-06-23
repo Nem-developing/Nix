6fe2cda1691c616c6eca599df360b820098dc38a
Néhémie <54665744+Nem-developing@users.noreply.github.com>
2020-06-23 22:57:49 +0200

    + Arguments de lancement du serveur.

--- config/config.php
+++ config/config.php
@@ -3,6 +3,9 @@
 /*  
  *  =========================================================================
  *  Veuillez spécifiez les informations de connection à votre base de donnée.
+ * 
+ *  Besoin d'aide ? Consultez la documentation !
+ *  https://github.com/Nem-developing/minecraft-web-srv/wiki#base-de-donn%C3%A9es-
  *  =========================================================================
  */ 
 
@@ -10,6 +13,26 @@ $hotedeconnexion = "127.0.0.1"; // 127.0.0.1 = Localhost
 $basededonnee = "MW-SRV";
 $utilisateur = "MW-SRV-USER";
 $motdepasse = "MW-SRV-PASSWORD";
+
+
+/*  
+ *  ===================================================================================
+ *  Veuillez spécifiez les arguments de lancement par défaut des nouveaux sereveurs.
+ * 
+ *  Par défaut 1Go de RAM est alloué.
+ * 
+ *  Les arguments : 
+ * 
+ *  -Xms = Allocation de mémoire au démarrage du programe.
+ *  -Xmx = Mémoire maximal que le programe pourra utiliser.
+ * 	
+ *  Vous pouvez par exemple allouer 512Mo au démarrage & 1Go maximal pour le serveur.
+ *  ---> "-Xms512M -Xmx1G"
+ *  ===================================================================================
+ */ 
+
+
+$commandedelancement = "-Xms1G -Xmx1G";
 
 
 ?>
