1e834c3abb96f820d702d1b2c3d17b7c136218f9
Néhémie <54665744+Nem-developing@users.noreply.github.com>
2020-06-24 03:39:07 +0200

    Ajout de la requête SQL avec variable.

--- actions/server-properties.php
+++ actions/server-properties.php
@@ -4,11 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-        <link href="../css/server-properties.css" rel="stylesheet" type="text/css"/>
-        <title>Modification d'un fichier server.properties !</title>
+        <title>Modifier un serveur</title>
     </head>
     <body>
-
         <!-- Menu -->
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <a class="navbar-brand" href="#">Minecraft Web Srv</a>
@@ -34,46 +32,105 @@
                 <a class="version" href="https://github.com/nem-developing/">MW-SRV 1.0 - Nem-Developing</a>
             </div>
         </nav>  
+        
+        <?php
+        include "../config/config.php"; // Import des données de connexion.
+        $id = $_GET['id'];
+        
+        // Si un utilisateur tente d'accéder à cette page sans passer par les boutons adaptés, alors on va l'éjecter pour ne pas qu'il
+        // y ait un soucis lors des requettes.
+        if (!$_GET['id']){
+            echo 'Erreur, vous de devez pas être là !';
+            header('Location: ../index.php');   // redireciton vers la page d'acceuil.
+            exit();
+        }
+        
+        //  Test de connexion à la base de donnée.
+        $mysqli = new mysqli("$hotedeconnexion", "$utilisateur", "$motdepasse", "$basededonnee");
+        if ($mysqli->connect_errno) {
+            echo "<div class='alert alert-danger' role='alert'> Echec lors de la connexion à MySQL ! </div>";   // Affichage de l'erreur.
+            echo "<div class='alert alert-danger' role='alert'> Erreur N°$mysqli->errno : $mysqli->error.</div>";    // Affichage de l'erreur.
+            $erreur = $erreur + 1;
+        }
+        
+        
+        // Récupération des informations du serveur dans les tables de données.
+        // Établissement de la connexion au serveur mysql.
+        $cnx = new PDO("mysql:host=$hotedeconnexion;dbname=$basededonnee", "$utilisateur", "$motdepasse");
+        // Commande SQL permetant de récupérer la liste des serveurs actifs.
+        $req = 'SELECT * FROM serveurs where id = "'.$id.'"';
+        echo "$req";
+        // Envoie au serveur la commande via le biais des informations de connexion.
+        $res = $cnx->query($req);
 
-
-
-    <center>
-        <div id="page">            
-            <?php
-            // Si un utilisateur tente d'accéder à cette page sans passer par les boutons adaptés, alors on va l'éjecter.
-            if (!$_GET['id']) {
-                header('Location: ../index.php');   // redireciton vers la page d'acceuil.
-                exit();
-            }
-
+        // Boucle tant qu'il y a de lignes corespondantes à la requettes
+        while ($ligne = $res->fetch(PDO::FETCH_OBJ)) {
+            echo "$ligne->id";
             
-            include "../config/config.php"; // Import des données de connexion.
-            $id = $_POST['id'];
-            
-            
-            (int) $erreur = 0; // Variable qui controle l'érreur.
-            
-            //  Connexion à la base de donnée.
-            $mysqli = new mysqli("$hotedeconnexion", "$utilisateur", "$motdepasse", "$basededonnee");
-            if ($mysqli->connect_errno) {
-                echo "<div class='alert alert-danger' role='alert'> Echec lors de la connexion à MySQL ! </div>";   // Affichage de l'erreur.
-                echo "<div class='alert alert-danger' role='alert'> Erreur N°$mysqli->errno : $mysqli->error.</div>";    // Affichage de l'erreur.
-                $erreur = $erreur + 1;
-            }
-            
-            if ($erreur === 0) {    // test de la présence d'erreurs ou non.
-                echo "pas d'erreurs";
-                header('Location: ../index.php');
-                exit();
-            } else {
-                echo "<h1>Il semble y avoir une erreur, veuillez vous référer à l'alerte au dessus !</h1>";
-            }
-            ?>
+        }
+        
+        ?>
+        
+        
+        
 
 
+        <! -- Titre de la page -->
+        <div id="page">
+            <p class="h1 breakshire" id="titrepage">Modification d'un serveur</p>
 
+
+            <!-- Formulaire de création d'un nouveau serveur-->
+
+
+            <form action="../actions/server-properties.php" method="post">
+                <div class="form-group">
+                    <label for="exampleFormControlInput1">Nom du serveur</label>
+                    <input class="form-control form-control-lg" type="text" name="namesrv" value="Exemple : Nemixcraft" required>
+                </div>
+                <div class="form-group">
+                    <label for="exampleFormControlSelect2">Nombre maximum de joueurs</label>
+                    <select multiple class="form-control" id="exampleFormControlSelect2" name="joueursmax">
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                        <option>6</option>
+                        <option>7</option>
+                        <option>8</option>
+                        <option>9</option>
+                        <option>10</option>
+                        <option>15</option>
+                        <option>20</option>
+                        <option>30</option>
+                        <option>40</option>
+                        <option>50</option>
+                        <option>60</option>
+                        <option>70</option>
+                        <option>80</option>
+                        <option>90</option>
+                        <option>100</option>
+                        <option>200</option>
+                        <option>300</option>
+                        <option>400</option>
+                        <option>500</option>
+                        <option>600</option>
+                        <option>777</option>
+                    </select>
+                </div>
+                <button type="submit" class="btn btn-primary mb-2 boutonenvoie" value="ok">Créer le serveur</button>
+            </form>
         </div>
-    </center>
+    
+
+
+
+
+
+
+
+
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
